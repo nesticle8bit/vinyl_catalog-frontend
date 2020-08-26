8d1ac387b1797470ca2a38e00e790cdb79d8dde4
Julio Poveda <jjpoveda92@gmail.com>
2020-08-26 11:43:28 -0500

    working on save community

--- src/app/components/pages/community/community-add/community-add.component.ts
+++ src/app/components/pages/community/community-add/community-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-community-add',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./community-add.component.scss']
 })
 export class CommunityAddComponent implements OnInit {
+  public formGroup: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(100)]],
+      description: ['', [Validators.required, Validators.maxLength(300)]],
+      url: ['', [Validators.required, Validators.maxLength(50)]],
+      publishRule: [''],
+      commentRule: [''],
+      nsfw: [false],
+    });
+  }
+
+  createCommunity(): void {
+    console.log(this.formGroup.value);
   }
 
 }
